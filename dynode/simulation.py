Adding explicit possibility to use a fixed timestep in the integration. This options hands over the control of deciding a reasonable timestep to the end user to avoid numerical instability etc.

dynode/simulation.py
@@ -70,7 +70,7 @@ class Simulation:
         return partial(self._events.remove, event)
 
     #pylint: disable=invalid-name, protected-access
-    def simulate(self, t, store_dt, integrator='dopri5', **kwargs) -> int:
+    def simulate(self, t, store_dt, fixed_step=False, integrator='dopri5', **kwargs) -> int:
         """
         Step forward in time, `t` seconds while storing any stored variables and
          checking events every `store_dt` interval.
@@ -105,8 +105,16 @@ class Simulation:
 
         # Setup of solver
         solver = ode(func)
-        solver.set_integrator(integrator, **kwargs)
         solver.set_initial_value(y0, t=self._t)
+        
+        # Setup of integration scheme
+        if fixed_step:
+            kwargs.update({
+                'first_step': store_dt,
+                'rtol': np.inf,
+                'atol': np.inf
+            })
+        solver.set_integrator(integrator, **kwargs)
 
         # Store initial results
         if self._t == 0: